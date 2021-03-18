3153085d7210eca8393888cb0b95bac5ac364ade
Jishnu70055 <jishnudq70055@gmail.com>
2021-03-18 13:15:03 +0530

    ### added

--- hello/star.py
+++ hello/star.py
@@ -1,11 +1,13 @@
 for i in range(0,5): 
     print("i" * i)
     print(" * "*i)
+    print("############################################")
 i=5
 while i: 
     print("0"*i)
     print(" * "*i)
     i=i-1
+
 # k=10
 # for i in range(0,5):
 #     for j in range(0,k):
@@ -15,3 +17,4 @@ while i:
 #         print(" * "*i)
 #     # print(" ")
 #sjaflkdj  klasdjfl;kads dkjfalskdfa kdjfa lkjD a
+#
